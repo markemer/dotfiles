52b1d2e0dc17f83805f434b0cdace484ca57e26d
Mark Anderson <emer@emer.net>
2025-05-20 20:35:46 -0400

    More in an unending list of vi tweaks

---
+++ dot_config/nvim/lua/config/lazy.lua
@@ -0,0 +1,54 @@
+-- Bootstrap lazy.nvim
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+  if vim.v.shell_error ~= 0 then
+    vim.api.nvim_echo({
+      { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+      { out, "WarningMsg" },
+      { "\nPress any key to exit..." },
+    }, true, {})
+    vim.fn.getchar()
+    os.exit(1)
+  end
+end
+vim.opt.rtp:prepend(lazypath)
+
+-- Make sure to setup `mapleader` and `maplocalleader` before
+-- loading lazy.nvim so that mappings are correct.
+-- This is also a good place to setup other settings (vim.opt)
+vim.g.mapleader = " "
+vim.g.maplocalleader = "\\"
+
+vim.o.syntax = on
+vim.o.shiftwidth = 4
+vim.o.number = true
+vim.o.relativenumber = true
+vim.o.encoding = "utf-8"
+vim.o.modelines = 0
+vim.o.wrap = true
+vim.o.smartindent = true
+vim.o.expandtab = true
+vim.o.tabstop = 2
+vim.o.wildmenu = true
+vim.o.wildmode = "list:longest"
+vim.o.hidden = true
+vim.o.ttyfast = true
+vim.o.laststatus = 2
+vim.o.showmode = true
+vim.o.showcmd = true
+
+-- Setup lazy.nvim
+require("lazy").setup({
+  spec = {
+    -- import your plugins
+    { import = "plugins" },
+  },
+  -- Configure any other settings here. See the documentation for more details.
+  -- colorscheme that will be used when installing plugins.
+  install = { colorscheme = { "habamax" } },
+  -- automatically check for plugin updates
+  checker = { enabled = true },
+})
+
