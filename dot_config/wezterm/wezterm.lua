a80b7f6561d08198fea9c024d446d6277a727f91
Mark Anderson <emer@emer.net>
2025-06-02 19:03:09 -0400

    Wezterm updates, add tuist

--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -4,7 +4,10 @@ local wezterm = require 'wezterm'
 local config = wezterm.config_builder()
 
 -- (This is where our config will go)
-config.font = wezterm.font 'FiraCode Nerd Font Mono'
+config.initial_cols = 135
+config.initial_rows = 25
+
+config.font = wezterm.font 'IBM Plex Mono'
 config.font_size = 15
 config.audible_bell = "Disabled"
 -- Returns our config to be evaluated. We must always do this at the bottom of this file
