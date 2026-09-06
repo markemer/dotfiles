e204a37142f1e7aa570423f552757aff895e77d9
Mark Anderson <emer@emer.net>
2026-09-05 20:07:06 -0400

    Make changes for linux

--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -7,7 +7,7 @@ local config = wezterm.config_builder()
 config.initial_cols = 135
 config.initial_rows = 25
 
-config.font = wezterm.font 'IBM Plex Mono'
+config.font = wezterm.font 'BlexMono Nerd Font'
 config.font_size = 15
 config.audible_bell = "Disabled"
 -- Returns our config to be evaluated. We must always do this at the bottom of this file
